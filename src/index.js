fedd30b4e75df4b13d683e2a449b9a7b4fc05cf4
tavira <quramolt@gmail.com>
2019-02-26 21:15:27 +0300

    Complete step3 with existing lint problems for travis

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const greetUser = () => {
-    const userName = askUserName();
-    console.log(`Hello, ${userName}!`);
-};
-
 const askUserName = () => {
-   const userName = readlineSync.question('May I have your name? ');
-   return userName;
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
 };
 
-export { greetUser };
+export default () => {
+  const userName = askUserName();
+  console.log(`Hello, ${userName}!`);
+};
